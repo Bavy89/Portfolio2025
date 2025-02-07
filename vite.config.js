c3c4baaf6e9fd17a38c0bed7d589d4a719a4fd89
Bave Ado <85221462+Bavy89@users.noreply.github.com>
2025-02-07 17:00:39 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bave-portfolio2025/",
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.pdf'],
-  build: {
-    assetsDir: 'assets'
-  }
+  base: '/',
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.pdf']
 });
